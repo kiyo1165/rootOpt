aa439b3d1844449c0563f4b6a4e94fe202711e83
akashi <kiyotsugu.send@gmail.com>
2023-05-20 22:49:59 +0900

    Created Detail View

--- client/urls.py
+++ client/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ClientListView
+from .views import ClientListView, ClientDetailView
 
 app_name = "client"
 urlpatterns = [
-    path("", ClientListView.as_view(), name="client_list")
+    path("client_list", ClientListView.as_view(), name='list'),
+    path("client_detail/<int:pk>", ClientDetailView.as_view(), name='detail')
+
 ]
--- client/views.py
+++ client/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Client
 # Create your views here.
 
 class ClientListView(ListView):
     model = Client
-    template_name = "templates/client_list.html"
+    template_name = "client/client_list.html"
     context_object_name = "client_list"
+
+class ClientDetailView(DetailView):
+    model= Client
+    template_name = "client/client_detail.html"
+    context_object_name = "client_detail"
